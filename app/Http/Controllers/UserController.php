63617ba9744ed73859b056be86af927bd2326aab
kelly <k.vargas.c31@hotmail.com>
2018-11-13 22:23:32 -0500

    Users controller and view

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends BaseController
+{
+    /**
+     * Create a controller instance.
+     *
+     * @param User $entity
+     */
+    public function __construct(User $entity)
+    {
+        parent::__construct($entity);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
---
+++ resources/views/app/users.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kelly Vargas
+ * Date: 13/11/2018
+ * Time: 10:16 PM
+ */
